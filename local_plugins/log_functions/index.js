f26563265b65e183d8b746193668c62dd6ec6a87
erezrokah <erezrokah@users.noreply.github.com>
2021-01-08 15:13:56 +0100

    chore: force user agent

--- local_plugins/log_functions/index.js
+++ local_plugins/log_functions/index.js
@@ -2,12 +2,11 @@ const axios = require(`axios`)
 
 module.exports = {
   async onPreBuild() {
-    const username = "289365524"
     const response = await axios.get(
-      `https://instagram.com/graphql/query/?query_id=17888483320059182&variables={"id":"${username}","first":100,"after":null}`
+      'https://instagram.com/graphql/query/?query_id=17888483320059182&variables={"id":"289365524","first":100,"after":null}',
+      { headers: { "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36" } }
     )
 
-    console.log(response)
     console.log(JSON.stringify(response.data, null, 2))
   },
 }
